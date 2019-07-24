f2209dc677bc181fd66b9bdfc59c2a8936b03f6b
pasanpeiris <pasan.peiris01@gmail.com>
2019-07-24 11:07:46 +0530

    contact us form

---
+++ contact_us.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" class="fa-events-icons-ready"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Contact Us</title>
+
+  
+          
+  <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+	<body>
+
+
+
+
+<!-- site content -->
+<div id="site-content" style="display: block;">
+
+<!-- site body -->
+
+<section id="section-page" class="container-fluid site-section">
+<div class="row">  
+
+<div class="col-lg-8 col-md-8 site-section-left">
+    
+  <div class="site-section-header">
+    <h3>CONTACT US</h3>
+  </div>
+
+  <div class="site-section-content">  
+  <div class="site-post-text">
+    <form action="php/sendMessage.php" method="post" class="">
+      <div class="form-group">
+          <label for="name">Your Name</label>
+          <input type="text" class="form-control site-form-elem" id="name" required="" name="name">
+      </div>
+      <div class="form-group">
+          <label for="email">Your Email</label>
+          <input type="email" class="form-control site-form-elem" id="email" required="" name="email">
+      </div>
+      <div class="form-group">
+          <label for="subject">Subject</label>
+          <input type="text" class="form-control site-form-elem" id="subject" required=""  name="subject" >
+      </div>
+      <div class="form-group">
+      <label for="subject">Your Message</label>
+      <textarea name="your-message" cols="40" rows="10" class="form-control site-form-textarea site-form-elem" aria-invalid="false" required=""></textarea>
+      </div>
+      <div class="form-group">
+      <input type="submit" value="SEND" class="form-control site-form-submit-btn">
+      </div>
+    </form>
+  </div>  
+  </div>
+
+
+ 
+
+
+</div>
+</section>
+</body>
---
+++ php/sendMessage.php
@@ -0,0 +1,51 @@
+<?php
+
+   // validation expected data exists
+   if(!isset($_POST['email']) ||
+   !isset($_POST['name']) ||
+   !isset($_POST['subject']) ||
+   !isset($_POST['your-message'])){
+    echo "<script type='text/javascript'>
+    alert('We are sorry, but there appears to be a problem with the form you submitted.');
+    window.location.href='../contact_us.php';
+    </script>";  
+    }
+
+
+$to = "pasan1486381@gmail.com";
+$subject = $_POST['subject'];
+$message = $_POST['your-message'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+$headers = "From: webmaster@obasystem.com";
+$error_message = "";
+$email_message="";
+$string_exp = "/^[A-Za-z .'-]+$/";
+ 
+if(!preg_match($string_exp,$name)) {
+  $error_message .= 'The  Name you entered does not appear to be valid';
+}
+
+if(strlen($error_message) > 0) {
+    echo "<script type='text/javascript'>alert('".$error_message."');window.location.href='../contact_us.php';</script>";
+}
+
+ 
+function clean_string($string) {
+    $bad = array("content-type","bcc:","to:","cc:","href");
+    return str_replace($bad,"",$string);
+  }
+
+
+  $email_message .= "Name: ".clean_string($name)."\r\n";
+  $email_message .= "Email: ".clean_string($email)."\r\n";
+  $email_message .= "Subject: ".clean_string($subject)."\r\n";
+  $email_message .= "Message: ".clean_string($message)."\r\n";
+
+mail($to,$subject,$email_message,$headers);
+echo "<script type='text/javascript'>
+alert('Thank you for contacting us. We will be in touch with you very soon.');
+window.location.href='../contact_us.php';
+</script>";  
+
+?>
