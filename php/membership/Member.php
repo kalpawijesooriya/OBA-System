82592aee0e7ca68149030bba93b128d1cf5de6ad
Dumindu De Silva <Dumidumadura@gmail.com>
2019-07-24 11:49:16 +0530

    Pending Registration list implemented with test data

--- php/membership/Member.php
+++ php/membership/Member.php
@@ -12,6 +12,32 @@ class Member extends Dbh{
     
         return $result;
     }
+
+    function getRegesteredMembers(){
+        $query ="SELECT * FROM member";
+        $result =$this->connect()->query($query);   
+        return $result;
+    }
+}
+
+class Dbh{
+    
+    private $serverName;
+    private $userName;
+    private $password;
+    private $dbName;
+
+   protected function connect()
+   { 
+       $this->serverName="us-cdbr-iron-east-02.cleardb.net";
+       $this->userName="b3ddc9c03fcdea";
+       $this->password="c72fb202";
+       $this->dbName="heroku_c89e249aac6f9c4";
+
+      $conn=new mysqli($this->serverName, $this->userName,$this->password,$this->dbName);
+      return $conn;
+        
+   }
 }
     
 ?>
--- php/membership/memberHedder.php
+++ php/membership/memberHedder.php
@@ -2,10 +2,13 @@
 require_once 'Member.php';
 
 if( $_POST['method']=="register" ) {
-
      $member = new Member();
      $result = $member->saveMemberData($_POST['regestration_number'],$_POST['nic'],$_POST['title'],$_POST['name'],$_POST['birthday'],$_POST['address'],$_POST['country'],$_POST['phone'],$_POST['mobile'],$_POST['email'],$_POST['index_num'],$_POST['ol_year'],$_POST['al_year'],$_POST['password']);
      echo $result;
+}
+else if($_POST['method']=="getRegisteredMembers" ) {
+     $member = new Member();
+     $result = $member->getRegesteredMembers();
 }
 
 ?>
