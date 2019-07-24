7e3c26c3826ddf11767c59e17b2408cf444237b6
Benuka Withanage <withanag_im14048@stu.kln.ac.lk>
2019-07-24 14:38:14 +0530

    Gallery Updated

--- image-gallery.php
+++ image-gallery.php
@@ -62,55 +62,17 @@
 </div>
 
 <div class="site-section-content">  
-
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/11523522.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/11523522.jpg" alt="dsscoba-image-gallery"/>
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/16295128.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/16295128.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/51613565.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/51613565.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/51461905.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/51461905.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/66367444.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/66367444.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/67973048.JPG" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/67973048.JPG" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/67172394.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/67172394.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/85073820.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/85073820.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/90947854.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/90947854.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/49495661.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/49495661.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/40234062.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/40234062.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/98369372.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/98369372.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/36471321.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/36471321.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/67645659.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/67645659.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/83684222.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/83684222.jpg" alt="dsscoba-image-gallery" />
-  </a>
-  <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="uploads/events/images/60283961.jpg" data-lightbox="site-album-set" data-title="Image-Gallery">
-      <img class="site-album-item" src="uploads/events/images/60283961.jpg" alt="dsscoba-image-gallery" />
-  </a>
+  <?php
+       		 $dir = glob('img/gallery/{*.png,*.jpg}',GLOB_BRACE);
+        
+            foreach($dir as $value){
+             ?>
+                <a class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-site-xs-12 site-grid-base-3 site-grid-bx site-album-image" href="<?php echo $value; ?>" data-lightbox="site-album-set" data-title="Image-Gallery">
+                    <img class="site-album-item" src="<?php echo $value; ?>"/>
+                </a>
+             <?php
+                    }
+                ?>
 
 </div>
 
