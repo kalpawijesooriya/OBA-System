63f9f40454e11779c0062b6dd247b077e936f544
akalanka345 <akalanka345@gmail.com>
2019-07-30 10:46:47 +0530

    upgrade to pro buttom removed

--- Admin/gallery.php
+++ Admin/gallery.php
@@ -80,7 +80,7 @@
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
                         <h4 class="page-title">Gallery - Administrator</h4> </div>
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-                        <a href="#" target="_blank" class="btn btn-danger pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Upgrade to Pro</a>
+                        
                         <ol class="breadcrumb">
                             <li><a href="#">Dashboard</a></li>
                             <li class="active">Gallery</li>
